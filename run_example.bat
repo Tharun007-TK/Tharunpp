ce4f8e8fca9ca9340a3f48ebc26e945df54f9b53
Tharun Kumar <tharunkumarvmt@gmail.com>
2026-03-29 13:19:07 +0530

    chore: clean up repo, regenerate lockfile

--- run_example.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Activating virtual environment and running hello.tpp...
-call venv\Scripts\activate.bat
-python -m Tharunpp.cli.main run-file examples\hello.tpp
-pause
--- tharunpp_cli.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-"""Simple entry point for Tharun++"""
-
-if __name__ == "__main__":
-    from Tharunpp.cli.main import app
-    app()
